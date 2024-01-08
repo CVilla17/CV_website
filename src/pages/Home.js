a6f2a9310f95a4edea944e6fe49dac3b2f5e86a9
Carlos Villa <villac@mit.edu>
2024-01-08 18:53:34 -0500

    kinda navbar and added files for pages

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+// import Todos from "../component/todos";
+// import AddTodo from "../component/AddTodo";
+import "../pages/Home.css";
+
+class Home extends Component {
+  // Create a default state of this component with an empty list of todos.
+
+  render() {
+    return <div className="Home">this is where i will put the home page</div>;
+  }
+}
+
+export default Home;
