eee48ef9e3174d9a7a0d4b713fa5c81924292c7f
Carlos Villa <villac@mit.edu>
2024-01-12 16:06:05 -0500

    Navbar woohoo, and text on home page

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -4,20 +4,24 @@ import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 //import Interests from "../pages/Interests";
 import Home from "../pages/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class NavbarComp extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Navbar bg="dark" variant={"dark"} expand="lg">
+          <Navbar bg="primary" variant={"dark"} expand="lg">
             <Container>
-              {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+              <Navbar.Brand href="#home">Explore</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
                   <Nav.Link as={Link} to={"/"}>
                     Home
+                  </Nav.Link>
+                  <Nav.Link as={Link} to={"/projects"}>
+                    Projects
                   </Nav.Link>
                   <Nav.Link as={Link} to={"/interests"}>
                     Interests
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,55 @@ import React, { Component } from "react";
 // import Todos from "../component/todos";
 // import AddTodo from "../component/AddTodo";
 import "../pages/Home.css";
+import { ListSubheader } from "@mui/material";
 
 class Home extends Component {
   // Create a default state of this component with an empty list of todos.
 
   render() {
-    return <div className="Home">this is where i will put the home page</div>;
+    return (
+      <div>
+        {/* <p>Design your About me page </p> */}
+        <div class="split left">
+          <div className="centered">
+            {/* <img
+              className="profile_image"
+              src={profile_pic}
+              alt="Profile Pic"
+            ></img> */}
+            <div>
+              <b>Quick Facts</b>
+              <div text-align="left">
+                <ul>
+                  <li>
+                    Grew up in Illinois and went to Warren Township High School
+                  </li>
+                  <li>
+                    Currently a junior at MIT studying Electrical Engineering
+                    and Computer Science
+                  </li>
+                  <li>
+                    Love for running, working out, cooking, poetry, and friend
+                    and family
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Carlos Villa</div>
+              <div className="brief_description">
+                Hi! Welcome to my website, I made this to show off things I've
+                worked on and I'm passionate about. Feel free to take a look
+                around!
+              </div>
+              <br></br>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
